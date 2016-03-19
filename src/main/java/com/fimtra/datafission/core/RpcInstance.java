657cdc58100c6375aa2032d19e0ac22b174c6af8
Ramon Servadei <ramon.servadei@gmail.com>
2016-03-19 17:11:16 +0000

    Add RPC name into warning message

--- src/main/java/com/fimtra/datafission/core/RpcInstance.java
+++ src/main/java/com/fimtra/datafission/core/RpcInstance.java
@@ -311,8 +311,8 @@ public final class RpcInstance implements IRpcInstance
                         }
                         if (ContextUtils.isCoreThread() || ContextUtils.isRpcThread())
                         {
-                            Log.log(this,
-                                "WARNING: RPC being called using a core/RPC thread - this can lead to a stall.");
+                            Log.log(this, "WARNING: RPC ", this.rpcName,
+                                " is being called using a core/RPC thread - this can lead to a stall.");
                         }
                         this.callReceiver.sendAsync(this.codec.getTxMessageForRpc(this.rpcName, args, resultMapName));
 
