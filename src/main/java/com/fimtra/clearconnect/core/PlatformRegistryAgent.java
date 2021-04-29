56ced1d153c32abc264d2ed0363fff6525906d27
Ramon Servadei <ramonservadei@users.noreply.github.com>
2021-04-29 10:05:23 +0100

    Add runtime process name (pid@host) into agent name if its not already there

--- src/main/java/com/fimtra/clearconnect/core/PlatformRegistryAgent.java
+++ src/main/java/com/fimtra/clearconnect/core/PlatformRegistryAgent.java
@@ -112,6 +112,16 @@ public final class PlatformRegistryAgent implements IPlatformRegistryAgent
         }
     }
 
+    private static String addProcId(String agentName)
+    {
+        final String processName = ManagementFactory.getRuntimeMXBean().getName();
+        if (!agentName.contains(processName))
+        {
+            return agentName + "_" + processName;
+        }
+        return agentName;
+    }
+
     final long startTime;
     final String agentName;
     final String hostQualifiedAgentName;
@@ -195,7 +205,7 @@ public final class PlatformRegistryAgent implements IPlatformRegistryAgent
     {
         Log.log(this, "Registry addresses: ", Arrays.toString(registryAddresses));
         this.startTime = System.currentTimeMillis();
-        this.agentName = agentName + "-" + new FastDateFormat().yyyyMMddHHmmssSSS(System.currentTimeMillis());
+        this.agentName = addProcId(agentName) + "_" + new FastDateFormat().yyyyMMddHHmmssSSS(System.currentTimeMillis());
         this.hostQualifiedAgentName = PlatformUtils.composeHostQualifiedName(this.agentName);
         this.createLock = new ReentrantLock();
         this.destroyCalled = new AtomicBoolean(false);
@@ -787,6 +797,7 @@ public final class PlatformRegistryAgent implements IPlatformRegistryAgent
                             (proxy != null ? " (proxy is inactive)" : ""));
                     return null;
                 }
+                // todo this must be an RPC
                 Map<String, IValue> serviceInfoRecord = this.registryProxy.getRemoteRecordImage(
                         ServiceInfoRecordFields.SERVICE_INFO_RECORD_NAME_PREFIX + serviceInstanceId,
                         getRemoteRecordImageTimeoutMillis());
@@ -977,6 +988,7 @@ public final class PlatformRegistryAgent implements IPlatformRegistryAgent
                 Log.log(this, "Registry has no service registered for '", serviceFamily, "'");
                 return null;
             }
+            // todo we need to have a new RPC to get the record for the service
             return this.registryProxy.getRemoteRecordImage(instanceForService.textValue(),
                     getRemoteRecordImageTimeoutMillis());
         }
